033f11e8d7d94a2ae0d631b1c95e5b3080c4b3cc
Katie Huang <whycatiadm@gmail.com>
2025-05-01 14:48:39 +0800

    V132 內測版本

--- components/HamburgerMenu.tsx
+++ components/HamburgerMenu.tsx
@@ -27,7 +27,7 @@ const HamburgerMenu = ({
 
   return (
     <View style={styles.menuContainer}>
-      <Text style={styles.menuItem}>版本: v1.3.0</Text>
+      <Text style={styles.menuItem}>版本: v1.3.2</Text>
 
       <TouchableOpacity
         onPress={() => { onClose();   toggleTheme(); }}
