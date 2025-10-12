369115da3f1dfc3c934c31f9856b925bc4dfc397
Katie Huang <whycatiadm@gmail.com>
2025-10-12 19:56:41 +0800

    V157B  升級成功

--- constants/variant.ts
+++ constants/variant.ts
@@ -3,7 +3,7 @@ export const APP_VARIANT: string = 'notedebug'; // 可切換為 'note'、'clamp'
 export const nginxVersion: string = 'green'; // 可切換為 'green(V1)'、'blue' 等  blue 的 whisper 還是 wav 要 copy green
 export const APP_TITLE = 'Voice Note';
 
-export const version : string = '1.5.7.A';
+export const version : string = '1.5.7.B';
 export const debugValue: string  = '1';  // 可切換 1 debug, 0 release
 
 export let SEGMENT_DURATION = 600; // default: 10 分鐘 設定在漢堡選單
